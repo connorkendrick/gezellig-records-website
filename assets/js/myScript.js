52e50b7aef0cf1ca5444dce7af5fc66471d7058b
Connor Kendrick <connorkendrickmusic@gmail.com>
2017-06-05 04:30:41 -0400

    keep nav menu open when scrolling

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -33,7 +33,6 @@ $(document).ready(function(){
                 $(".nav-button").attr('src',"{{ site.baseurl }}/assets/img/nav-button-close.svg");
               else
                 $(".nav-button").attr('src',"{{ site.baseurl }}/assets/img/nav-button-open.svg");
-              $("html").scroll(function(e){ e.preventDefault()});
           });
       });
     });
